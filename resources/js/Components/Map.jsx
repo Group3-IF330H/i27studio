0b4c83f40d59b1e03a9b455fcc448be44bef9cc7
Rilim06 <r.marvinlim@gmail.com>
2024-02-26 21:37:19 +0700

    Build: contact

---
+++ resources/js/Components/Map.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100%',
+  height: '100%',
+};
+
+const center = {
+  lat: -6.242469,
+  lng: 106.521669,
+};
+
+const Map = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyBgRomQkQX5NEvozGuoYDT3MwYY-8aeDfQ',
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
+  return (
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={18}
+        center={center}
+        options={{
+          disableDefaultUI: true,
+          mapTypeControl: false,
+        }}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+  );
+};
+
+export default Map;
