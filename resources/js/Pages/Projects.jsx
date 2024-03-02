7d91fb727dc1779254ab1d1b243a2de7d52b644f
Salma Manda <salmamanda03@gmail.com>
2024-03-02 07:20:46 +0700

    Build: projects

--- resources/js/Pages/Projects.jsx
+++ resources/js/Pages/Projects.jsx
@@ -1,10 +1,38 @@
 import PageLayout from "@/Layouts/PageLayout";
 import ContainerLayout from "@/Layouts/ContainerLayout";
 import React from "react";
+import SearchFilterComponent from "@/Components/SearchFilterComponent";
+import FilterButton from "@/Components/FilterButton";
+import { useState, useEffect, useMemo } from "react";
+import { debounce } from "lodash";
 
 const Projects = (props) => {
-    const project = props.data;
-    console.log("Props received by Projects component:", props);
+    let project = props.data;
+    // console.log("Props received by Projects component:", props);
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+    if (searchTerm !== "") {
+        project = project.filter((project) => {
+            return project.nama_project
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase());
+        });
+    }
+
+    const debouncedResults = useMemo(() => {
+        return debounce(handleChange, 700);
+    }, []);
+
+    useEffect(() => {
+        return () => {
+            debouncedResults.cancel();
+        };
+    });
 
     return (
         <>
@@ -20,11 +48,22 @@ const Projects = (props) => {
                                 reshape futures.
                             </p>
                         </div>
+                        <div className="grid grid-cols-5 gap-4 mt-8">
+                            <div className="col-span-4">
+                                <SearchFilterComponent
+                                    project={project}
+                                    debounce={debouncedResults}
+                                />
+                            </div>
+                            <div className="col-span-1 h-10">
+                                <FilterButton />
+                            </div>
+                        </div>
                         <div>
                             <div className="grid grid-cols-2 gap-4 mt-8">
                                 {project.map((project, index) => (
                                     <div className="relative h-96" key={index}>
-                                        <div className="absolute inset-0 bg-gradient-to-t from-black from-1%  to-transparent to-99% opacity-70 overlay"></div>
+                                        <div className="absolute inset-0 bg-gradient-to-t from-black from-1% to-transparent to-99% opacity-70 overlay"></div>
                                         <img
                                             src={`../storage/img/projects/${project.gambar_project}`}
                                             alt="Studio I27 Highlighted Project"
