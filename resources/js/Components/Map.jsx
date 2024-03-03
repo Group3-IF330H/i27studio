18d72747055b752886cd8b3beac5a7045940e80f
muhamaddafa <gaming102dafa@gmail.com>
2024-03-03 13:44:57 +0700

    fix: env and maps api key

--- resources/js/Components/Map.jsx
+++ resources/js/Components/Map.jsx
@@ -1,44 +1,44 @@
-import React from 'react';
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
-const libraries = ['places'];
+const libraries = ["places"];
 const mapContainerStyle = {
-  width: '100%',
-  height: '100%',
+    width: "100%",
+    height: "100%",
 };
 
 const center = {
-  lat: -6.242469,
-  lng: 106.521669,
+    lat: -6.242469,
+    lng: 106.521669,
 };
 
 const Map = () => {
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyBgRomQkQX5NEvozGuoYDT3MwYY-8aeDfQ',
-    libraries,
-  });
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: import.meta.env.VITE_MAPS_API_KEY,
+        libraries,
+    });
 
-  if (loadError) {
-    return <div>Error loading maps</div>;
-  }
+    if (loadError) {
+        return <div>Error loading maps</div>;
+    }
 
-  if (!isLoaded) {
-    return <div>Loading maps</div>;
-  }
+    if (!isLoaded) {
+        return <div>Loading maps</div>;
+    }
 
-  return (
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={18}
-        center={center}
-        options={{
-          disableDefaultUI: true,
-          mapTypeControl: false,
-        }}
-      >
-        <Marker position={center} />
-      </GoogleMap>
-  );
+    return (
+        <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            zoom={18}
+            center={center}
+            options={{
+                disableDefaultUI: true,
+                mapTypeControl: false,
+            }}
+        >
+            <Marker position={center} />
+        </GoogleMap>
+    );
 };
 
 export default Map;
